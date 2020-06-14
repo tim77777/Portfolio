23d68cbd7587978f90b1fa4e49745e12ff559b96
tim77777 <tabrown6@bigpond.com>
2020-06-14 12:36:49 +1000

    Added comments to second.sh

--- week1/second.sh
+++ week1/second.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#Prints to the standard output (screen)
 echo "Hi there!"
 echo "its good to see you $1!"
+#Provide an exit status of 0 to signify that the script has ended normally
 exit 0
