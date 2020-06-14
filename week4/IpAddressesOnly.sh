2cf5856cff60d90bafb1adc6622ab09b8c347da4
tim77777 <tabrown6@bigpond.com>
2020-06-14 12:40:47 +1000

    Added comments to IPAddressesOnly.sh

--- week4/IpAddressesOnly.sh
+++ week4/IpAddressesOnly.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+#run IpInfo.sh which provides a range of networking information, but pass it through a sed filter so that only the IP addresses are shown
 ./IpInfo.sh | sed -n '/IP Address/p' 
